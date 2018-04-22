0aabecf2b099005ed9c2594136b9aad1fe29cb58
sdw2330976 <sydhappy123@163.com>
2018-04-22 23:08:41 +0800

    test start transaction manual and annotation

--- src/main/java/com/sdw/soft/service/UserService.java
+++ src/main/java/com/sdw/soft/service/UserService.java
@@ -4,8 +4,13 @@ import com.sdw.soft.dao.UserDao;
 import com.sdw.soft.meta.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -18,13 +23,17 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
     public List<User> getAll() {
         return userDao.getAll();
     }
 
 
     public int noAnnotationMethod(User user) {
-        return addUser(user);
+        int count = addUser(user);
+        return count;
     }
     @Transactional(propagation = Propagation.REQUIRED)
     public int addUser(User user) {
@@ -46,7 +55,68 @@ public class UserService {
         return count;
     }
 
-    private final void test() {
+    public int manualInsert(User user) {
+        DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+        transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus transactionStatus = transactionManager.getTransaction(transactionDefinition);
+        try {
+            int count = userDao.addUser(user);
+            String manual = null;
+//            manual.length();
+            transactionManager.commit(transactionStatus);
+            return count;
+        } catch (TransactionException e) {
+            e.printStackTrace();
+            transactionManager.rollback(transactionStatus);
+        }
+        return 0;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void manualAndAnnotation(User user) {
+        userDao.addUser(user);
+        String annotation = null;
+        annotation.length();
+        user.setUserName("Annotation");
+        manualInsert(user);
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void insertRequired(User user) {
+        userDao.addUser(user);
+        user.setUserName("requires_new");
+
+        insertRequiresNew(user);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void insertRequiresNew(User user) {
+        String requiresNew = null;
+        userDao.addUser(user);
+        requiresNew.length();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void insertRequiredAndRequiresNewWithManual(User user) {
+        userDao.addUser(user);
+        user.setUserName("RequiresNew");
+        insertRequiresNewWithManual(user);
+    }
+
+    public void insertRequiresNewWithManual(User user) {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus transactionStatus = transactionManager.getTransaction(definition);
+        try {
+            String requiresNew = null;
+            userDao.addUser(user);
+            requiresNew.length();
+            transactionManager.commit(transactionStatus);
+        } catch (Exception e) {
+            e.printStackTrace();
+            transactionManager.rollback(transactionStatus);
+        }
 
     }
 }
--- src/test/java/com/sdw/soft/test/TransactionTest.java
+++ src/test/java/com/sdw/soft/test/TransactionTest.java
@@ -2,6 +2,7 @@ package com.sdw.soft.test;
 
 import com.sdw.soft.meta.User;
 import com.sdw.soft.service.UserService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,22 +20,25 @@ public class TransactionTest {
     @Autowired
     private UserService userService;
 
+    private User user = null;
+    @Before
+    public void setup() {
+        user = new User();
+        user.setAge(23);
+    }
     /**
      * 测试直接调用无@transactional注解的方法  此方法内部调用带@transactional注解的方法 注解不生效
      */
     @Test
     public void testNoAnnotationMethod() {
-        User user = new User();
         user.setUserName("NoAnnotation");
-        user.setAge(28);
         userService.noAnnotationMethod(user);
     }
     @Test
     public void testInsert() {
 //        System.out.println(JSONObject.toJSON(userService.getAll()));
-        User user = new User();
+
         user.setUserName("Jack");
-        user.setAge(28);
         int count = userService.addUser(user);
 //        int count = userService.insert(user);
         System.out.println(count);
@@ -43,8 +47,27 @@ public class TransactionTest {
 
 
     @Test
-    public void testManualTransaction() {
-
+    public void testManual() {
+        user.setUserName("manual");
+        userService.manualInsert(user);
     }
 
+    @Test
+    public void testManualAndAnnotation() {
+        user.setUserName("manualAndAnnotation");
+        userService.manualAndAnnotation(user);
+    }
+
+
+    @Test
+    public void testRequiredAndRequiresNew() {
+
+        user.setUserName("required");
+        userService.insertRequired(user);
+    }
+    @Test
+    public void testRequiredAndRequiresNewWithManual() {
+        user.setUserName("required");
+        userService.insertRequiredAndRequiresNewWithManual(user);
+    }
 }
